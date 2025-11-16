c8c130195b98f06e4bb659e9c0acbe3611f80f33
Simon <simonswart91@gmail.com>
2025-11-16 18:30:22 +0200

    df

--- index/next.config.ts
+++ index/next.config.ts
@@ -1,32 +1,7 @@
-import withPWA from "next-pwa";
+import type { NextConfig } from "next";
 
-const nextConfig = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: "public",       // service worker output folder
-    register: true,       // auto-register service worker
-    skipWaiting: true,    // immediately activate new SW
-    disable: process.env.NODE_ENV === "development", // disable PWA in dev
-    runtimeCaching: [
-      {
-        // Cache your images
-        urlPattern: /^\/frg.*\.png$/,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "images-cache",
-          expiration: { maxEntries: 20, maxAgeSeconds: 30 * 24 * 60 * 60 }, // 30 days
-        },
-      },
-      {
-        // Cache other static assets
-        urlPattern: /\.(?:js|css|html|json)$/,
-        handler: "StaleWhileRevalidate",
-        options: {
-          cacheName: "static-resources",
-        },
-      },
-    ],
-  },
-});
+const nextConfig: NextConfig = {
+  /* config options here */
+};
 
 export default nextConfig;
